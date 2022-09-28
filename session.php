c80ea27cef437606954af947dd21216b6e596de0
Isa Muhammed Alhaji <isamuhammad0131@gmail.com>
2022-09-28 09:31:27 +0100

    Fixed: Bug Loger Information

--- session.php
+++ session.php
@@ -4,6 +4,7 @@ if (isset($_SESSION['loger']) && !empty($_SESSION['loger'])) {
     $loger = $_SESSION['loger'];
     $department_id = $loger->department;
     $department_name = $loger->dpt_name;
+    $logerType = $loger->type;
     $academic_session = "2017/2018";
 } else {
     header('location:login.php');
