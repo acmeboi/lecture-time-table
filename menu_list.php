fd0046d33f252d860acf7a6c9dbb12d66014403c
Isa Muhammed Alhaji <isamuhammad0131@gmail.com>
2022-09-28 09:30:15 +0100

    Fixed: Sidebar Menu permission

---
+++ menu_list.php
@@ -0,0 +1,9 @@
+<a href="index.php" class="w3-border-bottom w3-border-green" >Dashboard</a>
+<a href="add_view_department.php" class="w3-border-bottom <?= $logerType != 0 ? '' : 'hide'?> w3-border-green">Manage Departments</a>
+<a href="reg_view_staff.php" class="w3-border-bottom  <?= $logerType != 0 ? 'hide' : ''?> w3-border-green" >Manage Staff</a>
+<a href="add_view_courses.php" class="w3-border-bottom  <?= $logerType != 0 ? 'hide' : ''?> w3-border-green">Manage Courses</a>
+<a href="time_table_schedulling.php" class="w3-border-bottom  <?= $logerType != 0 ? 'hide' : ''?> w3-border-green">Schedule Time Table</a>
+<a href="print_timetable.php" class="w3-border-bottom  <?= $logerType != 0 ? 'hide' : ''?> w3-border-green">Print Time Table</a>
+<a href="manage_users.php" class="w3-border-bottom  <?= $logerType != 0 ? '' : 'hide'?> w3-border-green">Manage Users</a>
+<a href="logout.php" class="w3-border-bottom w3-border-green">Logout</a>
+
